294e7528b5347fa3c666f067ea2d12eeed1a7d06
SylvieHsu <sylviehsu@outlook.com>
2019-06-04 22:09:29 +0800

    init

---
+++ src/pages/Index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Sidebar from "../components/Sidebar";
+
+class Index extends Component {
+  state = {};
+  render() {
+    return <Sidebar />;
+  }
+}
+
+export default Index;
